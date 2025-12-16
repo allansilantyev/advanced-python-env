f35212e0adf08a062c32cbba575f71a7e619cc05
Allan Silantyev <allansilantyev@gmail.com>
2025-12-16 17:09:26 +0500

    Week 1 tasks

---
+++ week 1/task1.py
@@ -0,0 +1,8 @@
+first_name = input("Allan")
+last_name = input("Silantyev")
+age = input("18")
+phone = input("87783751223")
+
+print(f"{first_name}, {last_name}")
+print(f"{age}")
+print(f"{phone}")
